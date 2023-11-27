bccb871cd8480cf6f2bb3b273a71604e7c2ba54c
Gabriel Xavier de Gouveia Osorio <gabriel.xavier@eqtlcontratada.com.br>
2023-11-27 17:50:54 -0300

    Teste utilizando python-dotenv

--- conection.py
+++ conection.py
@@ -1,10 +1,13 @@
 # arquivo de conexão do AD 
-
+import os
 from ldap3 import Connection
+from dotenv import load_dotenv
+
+load_dotenv()
 
 AD_SERVER = ""
 AD_USERNAME = ""
 AD_PASSWORD = ""
-TELEGRAM_BOT_TOKEN = ""
+TELEGRAM_BOT_TOKEN = "6479101603:AAHb4q4Sc6o_dBjaUVxeOzBISvz_zqmzwX4"
 con=Connection(AD_SERVER,AD_USERNAME,AD_PASSWORD, auto_bind=True)
 print(con)
